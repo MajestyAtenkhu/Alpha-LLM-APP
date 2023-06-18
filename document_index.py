62143312ceb63932ceedcf61430722d062b89ddd
Majesty Atenkhu <116476408+MajestyAtenkhu@users.noreply.github.com>
2023-06-18 18:33:05 -0400

    Update document_index.py
     # Create a BytesIO object from the response content
        file = BytesIO(response.content)
    
        # Create a PDF file reader object
        pdf_reader = PyPDF2.PdfFileReader(file)
    
        # Initialize an empty string to hold the text
        text = ""
    
        # Loop through each page in the PDF and extract the text
        for page_num in range(pdf_reader.getNumPages()):
            page = pdf_reader.getPage(page_num)
            text += page.extractText()

--- document_index.py
+++ document_index.py
@@ -1,13 +1,31 @@
 #document_index.py
-import requests
+
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
+import PyPDF2
+import requests
+from io import BytesIO
 
 def load_documents_and_create_index(document_url):
 
     # Download the document
     response = requests.get(document_url)
-    document_content = response.text
+    
+    # Create a BytesIO object from the response content
+    file = BytesIO(response.content)
+
+    # Create a PDF file reader object
+    pdf_reader = PyPDF2.PdfFileReader(file)
+
+    # Initialize an empty string to hold the text
+    text = ""
+
+    # Loop through each page in the PDF and extract the text
+    for page_num in range(pdf_reader.getNumPages()):
+        page = pdf_reader.getPage(page_num)
+        text += page.extractText()
+
+    
     # Load the documents
     loader = TextLoader(document_content)
 
