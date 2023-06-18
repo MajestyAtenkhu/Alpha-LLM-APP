85f290b5dacd76090e5cdd3b511922c76a87170b
Majesty Atenkhu <116476408+MajestyAtenkhu@users.noreply.github.com>
2023-06-18 17:23:42 -0400

    Update document_index.py
    Updated code to download document from a link before indexing it

--- document_index.py
+++ document_index.py
@@ -1,11 +1,15 @@
 #document_index.py
-
+import request 
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 
-def load_documents_and_create_index(document_path)
+def load_documents_and_create_index(document_url)
+
+    # Download the document
+    response = requests.get(document_url)
+    document_content = response.text
     # Load the documents
-    loader = TextLoader(document_path)
+    loader = TextLoader(document_content)
 
     # Create the index
     index = VectorstoreIndexCreator().from_loaders([loader])
