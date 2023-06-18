3b388046f574e3d836a400a1f2fe6e404182a19e
Majesty Atenkhu <116476408+MajestyAtenkhu@users.noreply.github.com>
2023-06-18 15:44:26 -0400

    Update document_index.py
    Functions Parameter can not be a string

--- document_index.py
+++ document_index.py
@@ -3,9 +3,9 @@
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 
-def load_documents_and_create_index(r'C:\Users\maten\Documents\Resume\Majesty Resume V 5.1.pdf')
+def load_documents_and_create_index(document_path)
     # Load the documents
-    loader = TextLoader(r'C:\Users\maten\Documents\Resume\Majesty Resume V 5.1.pdf')
+    loader = TextLoader(document_path)
 
     # Create the index
     index = VectorstoreIndexCreator().from_loaders([loader])
