af394c52081bbb89b234e5ad362180761ad111e0
Majesty Atenkhu <116476408+MajestyAtenkhu@users.noreply.github.com>
2023-06-18 18:46:37 -0400

    Update document_index.py
    pdfplumber replacing the deprecated PyPDF2

--- document_index.py
+++ document_index.py
@@ -2,7 +2,7 @@
 
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
-import PyPDF2
+import pdfplumber
 import requests
 from io import BytesIO
 
@@ -11,20 +11,12 @@ def load_documents_and_create_index(document_url):
     # Download the document
     response = requests.get(document_url)
     
-    # Create a BytesIO object from the response content
-    file = BytesIO(response.content)
+   with open('document.pdf', 'wb') as f:
+        f.write(response.content)
 
-    # Create a PDF file reader object
-    pdf_reader = PyPDF2.PdfFileReader(file)
-
-    # Initialize an empty string to hold the text
-    text = ""
-
-    # Loop through each page in the PDF and extract the text
-    for page_num in range(pdf_reader.getNumPages()):
-        page = pdf_reader.getPage(page_num)
-        text += page.extractText()
-
+    # Extract text from the PDF
+    with pdfplumber.open('document.pdf') as pdf:
+        text = '\n'.join(page.extract_text() for page in pdf.pages)
     
     # Load the documents
     loader = TextLoader(document_content)
