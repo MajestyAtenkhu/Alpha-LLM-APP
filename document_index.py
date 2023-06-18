a6b0d228a93c9938fa8539881dee6432396e47e2
Majesty Atenkhu <116476408+MajestyAtenkhu@users.noreply.github.com>
2023-06-18 19:32:16 -0400

    Update document_index.py
    indentation 1

--- document_index.py
+++ document_index.py
@@ -6,17 +6,14 @@ import pdfplumber
 import requests
 
 def load_documents_and_create_index(document_url):
-
     # Download the document
     response = requests.get(document_url)
-    
-   with open('document.pdf', 'wb') as f:
+    with open('document.pdf', 'wb') as f:
         f.write(response.content)
 
     # Extract text from the PDF
     with pdfplumber.open('document.pdf') as pdf:
         text = '\n'.join(page.extract_text() for page in pdf.pages)
-    
     # Load the documents
     loader = TextLoader(document_content)
 
