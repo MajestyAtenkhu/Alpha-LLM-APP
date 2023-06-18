9c266bb2cf383cafa4fcbe8933bacf6a5ae06623
Majesty Atenkhu <116476408+MajestyAtenkhu@users.noreply.github.com>
2023-06-18 15:13:45 -0400

    Update app.py
    Path to Document Replaced

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = api_key
 
 # Load your documents and create an index
-document_path = 'path_to_your_document'  # replace with the actual path to your document
+document_path = 'C:\Users\maten\Documents\Resume\Majesty Resume V 5.1.pdf'  # replace with the actual path to your document
 index = load_documents_and_create_index(document_path)
 
 def main():
